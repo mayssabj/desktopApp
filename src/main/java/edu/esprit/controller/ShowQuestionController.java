a4a5c77aace72aca877aa2fabeb74cee0ff1bb34
CharFounix <ashrefbouden@gmail.com>
2024-05-13 21:24:42 +0100

    fix achref merge

--- src/main/java/edu/esprit/controller/ShowQuestionController.java
+++ src/main/java/edu/esprit/controller/ShowQuestionController.java
@@ -25,7 +25,6 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
-import com.google.cloud.translate.*;
 
 
 public class ShowQuestionController {
