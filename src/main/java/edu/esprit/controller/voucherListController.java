4e2fe129bf583856d700502cf57f00dc8a26b53b
YASSCODER <mohamedyassine.benaoun@esprit.tn>
2024-05-13 23:22:54 +0100

    fix ui + update usibility voucher

--- src/main/java/edu/esprit/controller/voucherListController.java
+++ src/main/java/edu/esprit/controller/voucherListController.java
@@ -194,9 +194,17 @@ public class voucherListController implements Initializable {
     @FXML
     void pdf(MouseEvent event) throws SQLException, DocumentException, IOException, WriterException {
         Voucher selectedVoucher = voucherTable.getSelectionModel().getSelectedItem();
-        System.out.println(selectedVoucher.toString());
-        handleSaveFile(selectedVoucher);
-
+        if (selectedVoucher != null) {
+            System.out.println(selectedVoucher.toString());
+            try{
+                voucherDAO.updateVoucherState(selectedVoucher);
+            }catch(Exception e){
+                System.out.println(e.toString());
+            }
+            handleSaveFile(selectedVoucher);
+        } else {
+            System.out.println("No voucher selected");
+        }
     }
 
     public void displayVouchers() {
