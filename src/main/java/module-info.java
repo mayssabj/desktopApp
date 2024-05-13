bb6b27d2acbc6411a730c3a33afa8d6e7e5470fd
CharFounix <ashrefbouden@gmail.com>
2024-05-13 21:20:47 +0100

    merge achref

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,27 +4,45 @@ module edu.esprit {
     requires javafx.fxml;
     requires java.sql;
     requires jbcrypt;
-    exports edu.esprit.enums to com.google.gson;
-    requires com.google.gson;
-    requires javax.mail;
     requires java.persistence;
     requires javafx.web;
-    requires jdk.jsobject;
+    requires com.google.gson;
+    requires java.net.http;
     requires cloudinary.core;
+    exports edu.esprit.enums to com.google.gson;
+    requires jdk.jsobject;
     requires org.kordamp.bootstrapfx.core;
     requires twilio;
     requires itextpdf;
     requires java.desktop;
-    //requires httpcore;
-   // requires httpclient;
     requires httpmime;
     requires org.apache.httpcomponents.httpcore;
     requires org.apache.httpcomponents.httpclient;
-    requires google.cloud.translate;
+    requires java.datatransfer;
+
+    requires com.google.zxing;
+    requires com.google.zxing.javase;
+    requires javax.mail.api;
+    requires okhttp3;
+    requires google.api.translate.java;
+    requires spring.security.crypto;
 
     opens edu.esprit.controller.user to javafx.fxml;
     opens edu.esprit.controller to javafx.fxml;
+    // Add this line to open the Reclamation controller package to javafx.fxml
+    opens edu.esprit.controller.Reclamation to javafx.fxml;
+    // Add this line to open the Reclamation controller package to javafx.fxml
+    opens edu.esprit.controller.Avertissement to javafx.fxml;
+
+
     opens edu.esprit.entities to javafx.base;
 
     exports edu.esprit;
+    // Exporte le package contenant StatisticsController au module javafx.fxml
+    exports edu.esprit.controller.Statistique to javafx.fxml;
+
+    // Ouvre le package pour la réflexion par javafx.fxml
+    opens edu.esprit.controller.Statistique to javafx.fxml;
+
+
 }
